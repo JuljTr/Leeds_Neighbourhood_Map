1d881abaa23a106b90e976f513bf623055a68949
Juljeta Troka <juliet.troka@gmail.com>
2023-06-07 11:29:11 +0100

    added first draft of the map

---
+++ script.js/app.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+
+const map = L.map("map").setView([53.7947, -1.5025], 10);
+const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer( tileURL, { attribution }).addTo(map);
+
+console.log(map)
